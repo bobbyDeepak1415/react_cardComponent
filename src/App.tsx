ea9201fd19c42e696aebabf38323430c9f5e35ab
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-03-11 02:05:45 +0530

    setup done

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,12 @@
 import "./App.css";
 import Badge from "./components/Badge";
 
-
 function App() {
   return (
     <div className="App">
-<div className="card-container">
-
-      <Badge filled={true} text="New Post" />
-</div>
+      <div className="card-container">
+        <Badge filled={true} text="New Post" />
+      </div>
     </div>
   );
 }
--- src/components/Badge.tsx
+++ src/components/Badge.tsx
@@ -5,7 +5,7 @@ import styles from "./Badge.module.css";
 const Badge = ({ text, filled }: BadgeInterface) => {
   const filledClass = filled ? styles.filled : "";
 
-  return <small className={`${styles.badge} ${filledClass}`}>{text}</small>;
+  return <small  className={`${styles.badge} uppercase ${filledClass}`}>{text}</small>;
 };
 
 export default Badge;
