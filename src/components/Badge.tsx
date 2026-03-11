7a3040caff7c70f3ad59705b86359b9aa60f4856
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-03-12 02:49:10 +0530

    setup done

--- src/components/Badge.tsx
+++ src/components/Badge.tsx
@@ -5,7 +5,9 @@ import styles from "./Badge.module.css";
 const Badge = ({ text, filled }: BadgeInterface) => {
   const filledClass = filled ? styles.filled : "";
 
-  return <small  className={`${styles.badge} uppercase ${filledClass}`}>{text}</small>;
+  return (
+    <small className={`${styles.badge} uppercase ${filledClass}`}>{text}</small>
+  );
 };
 
 export default Badge;
